0288323ad11243805feb876c1de49ce625fb79ba
rthamaraiselvi143 <48053153+rthamaraiselvi143@users.noreply.github.com>
2019-08-13 08:33:38 -0700

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,19 @@
+ 
+#include <iostream> 
+  
+using namespace std; 
+   void sortit(int arr[], int n) 
+{ 
+    for (int i = 0; i < n; i++) { 
+      arr[i]=i+1; 
+    } 
+} 
+  
+int main() 
+{ 
+    int arr[] = { 10, 7, 9, 2, 8, 3, 5, 4, 6, 1 }; 
+    int n = sizeof(arr) / sizeof(arr[0]);  
+    sortit(arr, n);  
+    for (int i = 0; i < n; i++)  
+        cout << arr[i] << " ";     
+} 
